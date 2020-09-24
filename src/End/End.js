d93742f96eb7b1b1ccba73dd3e3f30d3b87edf28
MarianoRamborger <Marianoramborger@gmail.com>
2020-09-24 14:47:53 -0300

    v1.3

--- src/End/End.js
+++ src/End/End.js
@@ -7,8 +7,7 @@ const End = () => {
 
         <h2> "La meta es descubrir el infinito valor de ti mismo" </h2>
 
-        <h2> Por última vez, de nuevo en el presente, observé el pequeño teléfono y entendí la importancia de tener la historia del universo entre mis manos. Un pequeño Aleph a disposición de cada uno. El pasado de la humanidad, el presente de las redes sociales y los futuros jamás imaginados: Todo confluye simbólicamente en la potencialidad de cada uno, nuestro rol en el presente y nuestra capacidad para edificar un futuro.
-Ahí te mando los links</h2>
+        <h2> Por última vez, de nuevo en el presente, observé el pequeño teléfono y entendí la importancia de tener la historia del universo entre mis manos. Un pequeño Aleph a disposición de cada uno. El pasado de la humanidad, el presente de las redes sociales y los futuros jamás imaginados: Todo confluye simbólicamente en la potencialidad de cada uno, nuestro rol en el presente y nuestra capacidad para edificar un futuro.</h2>
 
         </div>
     )
